ad35716d6aaf838fd1e6c356fb0e2add9d9c91d2
Guilherme Marques Rizzatto Teixeira <guimarquesrz@gmail.com>
2023-09-13 23:34:44 -0300

    ADD: Workout

---
+++ src/main/java/com/guiprojects/academy/entities/Workout.java
@@ -0,0 +1,66 @@
+package com.guiprojects.academy.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Workout implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String description;
+	
+	public Workout() {
+	}
+
+	public Workout(Long id, String description) {
+		this.id = id;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Workout other = (Workout) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}
