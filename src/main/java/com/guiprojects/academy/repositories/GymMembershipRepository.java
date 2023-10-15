00be789abb4e7948005a3355214fd95f673acdd8
Guilherme Marques Rizzatto Teixeira <guimarquesrz@gmail.com>
2023-10-14 23:43:22 -0300

    ADD `findFullById` method in GymMembership API layer

--- src/main/java/com/guiprojects/academy/repositories/GymMembershipRepository.java
+++ src/main/java/com/guiprojects/academy/repositories/GymMembershipRepository.java
@@ -1,5 +1,7 @@
 package com.guiprojects.academy.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -18,5 +20,20 @@ public interface GymMembershipRepository extends JpaRepository<GymMembership, Lo
 			WHERE id = :memberId
 			""")
 	public void updateRegistration(Long registrationId, Long memberId);
+	
+	@Transactional
+	@Modifying
+	@Query(nativeQuery = true, value = """			
+			UPDATE gym_membership
+			SET workout_id = :workoutId
+			WHERE id = :memberId
+			""")
+	public void updateWorkout(Long workoutId, Long memberId);
+	
+	@Query("SELECT obj FROM GymMembership obj "
+			+ "JOIN FETCH obj.workout AS w "
+			+ "JOIN FETCH w.exercises "
+			+ "WHERE obj.id = ?1")
+	public Optional<GymMembership> findFullById(Long id);
 	
 }
