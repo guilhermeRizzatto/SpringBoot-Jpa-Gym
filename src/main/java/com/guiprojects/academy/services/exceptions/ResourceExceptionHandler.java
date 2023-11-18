2b9e21d2721d9b21c6eaec84a9f57f4a111d87c1
Guilherme Marques Rizzatto Teixeira <guimarquesrz@gmail.com>
2023-11-17 21:50:47 -0300

    ADD customs `exceptions`

---
+++ src/main/java/com/guiprojects/academy/services/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.guiprojects.academy.services.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.guiprojects.academy.controllers.exceptions.StandardError;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound (ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase (DataBaseException e, HttpServletRequest request){
+		String error = "DataBase error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(TrainerWorkLoadException.class)
+	public ResponseEntity<StandardError> trainerWorkLoad (TrainerWorkLoadException e, HttpServletRequest request){
+		String error = "Trainer error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+
+
+}
