07a7456d30aa20383d9999efadb2ca094c365474
guilhermeRizzatto <guimarquesrz@gmail.com>
2023-11-20 00:37:18 -0300

    MINOR FIXES

--- src/main/java/com/guiprojects/academy/repositories/ExerciseRepository.java
+++ src/main/java/com/guiprojects/academy/repositories/ExerciseRepository.java
@@ -1,28 +1,26 @@
 package com.guiprojects.academy.repositories;
 
-import java.util.Optional;
-import java.util.Set;
-
+import com.guiprojects.academy.entities.Exercise;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import com.guiprojects.academy.entities.Exercise;
-
-import jakarta.transaction.Transactional;
+import java.util.Optional;
+import java.util.Set;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long>{
 
-	@Query(nativeQuery = true, value = """			
+	@Query(nativeQuery = true, value = """
 			SELECT * FROM exercise
 			WHERE workout_id = :workoutId
 			""")
 	public Set<Exercise> findByWorkoutId(Long workoutId);
 	
-	@Query(nativeQuery = true, value = """			
+	@Query(nativeQuery = true, value = """
 			SELECT * FROM exercise
 			WHERE workout_id = :workoutId
-			AND exercise_type_id = :exerciseTypeId	
+			AND exercise_type_id = :exerciseTypeId
 			""")
 	public Optional<Exercise> findByIds(Long workoutId, Long exerciseTypeId);
 	
@@ -31,7 +29,7 @@ public interface ExerciseRepository extends JpaRepository<Exercise, Long>{
 	@Query(nativeQuery = true, value = """			
 			DELETE FROM exercise
 			WHERE workout_id = :workoutId
-			AND exercise_type_id = :exerciseTypeId	
+			AND exercise_type_id = :exerciseTypeId
 			""")
 	public int deleteByIds(Long workoutId, Long exerciseTypeId);
 }
